44bc1e533277b7ac526501d7dd146ef6fc4bf9b5
Alex Perez <alex27_perez@hotmail.com>
2026-07-14 10:11:21 -0600

    Initial controller tests

---
+++ TaskApp.Api/TaskApp.Tests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskApp.Tests.Controllers
+{
+    public class AuthControllerTests
+    {
+        [Fact]
+        public void AuthController_ShouldDoLogin()
+        {
+            var user = new UserDto()
+            {
+                Id = Guid.NewGuid(),
+                Username = "testuser",
+                Password = "",
+                FirstName = "Test",
+                LastName = "User",
+                Email = ""
+            };
+            
+            var controller = new AuthController();
+            var loginDto = new LoginDto()
+            {
+                User = "testuser",
+                Pass = "testpass",
+            };
+            var result = controller.Login(loginDto);
+
+            Assert.NotNull(result);
+            var innerResult = (result.Result as OkObjectResult).Value as LoginResponseDto;
+            Assert.Equal(System.Threading.Tasks.Task.CompletedTask.Status, result.Status);
+            Assert.True(innerResult!.Success);
+        }
+    }
+}
---
+++ TaskApp.Api/TaskApp.Tests/Controllers/TasksControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskApp.Tests.Controllers
+{
+    public class TasksControllerTests
+    {
+        [Fact]
+        public void TasksController_ShouldCreateTask()
+        {
+            var taskParam = new TaskDto
+            {
+                Name = "Test Task",
+                Description = "Test Description",
+                DueDate = DateTime.Now,
+                AssignedToId = Guid.NewGuid(),
+                StatusId = Guid.NewGuid(),
+            };
+            var expectedTask = new TaskDto
+            {
+                Id = Guid.NewGuid(),
+                Name = taskParam.Name,
+                Description = taskParam.Description,
+                DueDate = taskParam.DueDate,
+                AssignedToId = taskParam.AssignedToId,
+                StatusId = taskParam.StatusId,
+            };
+            var expectedResponse = new BaseResponseDto<TaskDto>
+            {
+                Data = expectedTask,
+                Success = true,
+                Message = "Task created successfully"
+            };
+            
+            var controller = new TaskController();
+            var result = controller.Create(taskParam);
+
+            Assert.NotNull(result);
+            var innerResult = (result.Result as OkObjectResult).Value as BaseResponseDto<TaskDto>;
+            Assert.Equal(System.Threading.Tasks.Task.CompletedTask.Status, result.Status);
+            Assert.True(innerResult!.Success);
+            Assert.NotEmpty(innerResult!.Data!.Id.ToString());
+        }
+    }
+}
